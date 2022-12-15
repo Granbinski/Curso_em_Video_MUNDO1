7c06b06bba35404eb4d0bc947c92a6f7197e2963
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-12-14 21:16:13 -0300

    update

---
+++ Exercicios/Exercicio_006.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um numero inteiro: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (1 / 2)
+print('O dobro de {} é {} e o triplo é {}. A raiz quadrada é {:.2f}'.format(
+    numero, dobro, triplo, raiz_quadrada))
