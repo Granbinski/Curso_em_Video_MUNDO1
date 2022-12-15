39f729a3be220365685008b885f3dada0501fcfb
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-12-14 21:12:31 -0300

    update

---
+++ Aula.py
@@ -0,0 +1,3 @@
+n1 = 10
+n2 = 20
+print('A soma entre {} e  {} \n é {}'.format(n1, n2, n1+n2), end=' ')
---
+++ Exercicios/Exercicio_005.py
@@ -0,0 +1,5 @@
+numero_int = int(input('Digite um numero inteiro: '))
+antecessor = numero_int - 1
+sucessor = numero_int + 1
+print('O antecessor de {} é {} e o sucessor é {}'.format(
+    numero_int, antecessor, sucessor))
